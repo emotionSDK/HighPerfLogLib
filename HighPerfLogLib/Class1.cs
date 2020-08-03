20102e82d3f7ea6936f780d4c59c998a06a45ca8
Justin A. Barnes <justin@lesllc.biz>
2020-08-02 23:37:41 -0500

    Head On!

---
+++ HighPerfLogLib/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.IO.MemoryMappedFiles;
+
+namespace HighPerfLogLib
+{
+    public static class MemoryOperations
+    {
+        public static MemoryMappedFile CreateFileHandle(string file, int length)
+        {
+            if (string.IsNullOrEmpty(file))
+                throw new ArgumentException("The input file cannot be empty.");
+            if (length < 0)
+                throw new ArgumentException("The input length cannot be less than zero.");
+
+            MemoryMappedFileAccess access = MemoryMappedFileAccess.ReadWrite;
+            MemoryMappedFileOptions options = MemoryMappedFileOptions.None;
+            HandleInheritability inheritability = HandleInheritability.Inheritable;
+
+            return MemoryMappedFile.CreateOrOpen(file, length, access, options, inheritability);
+        }
+
+        public static void WriteShMem(MemoryMappedFile file, byte[] buffer, int startPos, int length)
+        {
+            if (MemoryMappedFile.ReferenceEquals(file, null))
+                throw new ArgumentNullException(nameof(file), "The argument cannot be null");
+
+            using (MemoryMappedViewStream stream = file.CreateViewStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(buffer, startPos, length);
+                }
+            }
+        }
+
+        public static byte[] ReadShMem(MemoryMappedFile file, int startPos, int length)
+        {
+            if (MemoryMappedFile.ReferenceEquals(file, null))
+                throw new ArgumentException("The input file cannot be empty.");
+            if (startPos < 0)
+                throw new ArgumentException("The input startPos cannot be less than zero.");
+            if (length < 0)
+                throw new ArgumentException("The input length cannot be less than zero.");
+
+            byte[] bytes = new byte[length];
+            
+            using (MemoryMappedViewStream stream = file.CreateViewStream())
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    reader.Read(bytes, startPos, length);
+                }
+            }
+
+            return bytes;
+        }
+    }
+}
---
+++ ReadWriteMem/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.MemoryMappedFiles;
+
+/*
+ * Issue with MemoryMappedFile.CreateFromFile: https://github.com/dotnet/runtime/issues/16371
+ * -> CreateFromFile doesn't use sane defaults. It's suggested to create the filestream yourself and pass
+ *    it to CreateFromFile.
+ * 
+ * Of course, this doesn't matter when you use CreateOrOpen. At that point, you're just trying to make
+ * sure the mapName is unique to any other mutex, handle, etc, etc on the system.
+ */
+
+namespace ReadWriteMem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string memfile = "{B4ECD457-AF52-45DC-8E2D-790E18BA7A85}";
+                using (MemoryMappedFile mmf = HighPerfLogLib.MemoryOperations.CreateFileHandle(memfile, 1500))
+                {
+                    byte[] buffer = new byte[] { 0xDE, 0xAD, 0xBE, 0xEF };
+
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        Console.WriteLine(buffer[i]);
+                    }
+
+                    HighPerfLogLib.MemoryOperations.WriteShMem(mmf, buffer, 0, buffer.Length);
+
+                    byte[] buffer2 = null;
+
+                    buffer2 = HighPerfLogLib.MemoryOperations.ReadShMem(mmf, 0, buffer.Length);
+
+                    for (int i = 0; i < buffer2.Length; i++)
+                    {
+                        Console.WriteLine(buffer2[i]);
+                    }
+
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+    }
+}
